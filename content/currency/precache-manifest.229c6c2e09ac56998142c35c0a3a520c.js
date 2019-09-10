70aa93356ce4533198acfee88c513b330f8a894f
Valerii <45800342+Zinchenkoipad@users.noreply.github.com>
2019-09-10 23:12:48 +0200

    currency

---
+++ content/currency/precache-manifest.229c6c2e09ac56998142c35c0a3a520c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d34088c979a7d5d93dca81e9b77daec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adc6844f2bc3fe5fee8c",
+    "url": "/static/js/2.e0f4460f.chunk.js"
+  },
+  {
+    "revision": "eab973865c8b3b42924a",
+    "url": "/static/js/main.bee4caab.chunk.js"
+  },
+  {
+    "revision": "5ae09aeb9cc8a1535344",
+    "url": "/static/js/runtime~main.d79c1471.js"
+  }
+]);
